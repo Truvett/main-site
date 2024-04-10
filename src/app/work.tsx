5f336eaf7bbc13cac0b523262edd05b14a38069e
Truvettsolutions <165828507+Truvettsolutions@users.noreply.github.com>
2024-04-10 15:31:59 +0530

    removed BH projects

--- src/app/work.tsx
+++ src/app/work.tsx
@@ -21,41 +21,41 @@ export function Work() {
 				'Glazer Games stands as a pioneering force in the esports and gaming industry, driven by a passion for technology and a steadfast commitment to reshaping the esports landscape in India.',
 			link: 'https://www.glazer.games/',
 		},
-		{
-			image: '/work/giftlips.png',
-			title: 'GiftLips',
-			description:
-				'GiftLips takes the traditional greeting card experience to the next level.',
-			link: 'https://www.giftlips.com/',
-		},
-		{
-			image: '/work/heyweek.png',
-			title: 'Heyweek',
-			description:
-				'Heyweek, your freelancing toolbox. Simplify your tasks, supercharge your workflow, and thrive in your independent journey',
-			link: 'https://www.heyweek.com/',
-		},
-		{
-			image: '/work/yesspark.png',
-			title: 'YesSpark',
-			description:
-				'YesSpark inspire and encourage future entrepreneurs, founders and teams who are passionate about delivering unique solutions to their customers from Spark to Launch.',
-			link: 'https://www.yesspark.io/',
-		},
-		{
-			image: '/work/eurobridge.png',
-			title: 'Eurobridge',
-			description:
-				'EuroBridge Shipping Services deliver the right service, at the right level of quality, and for the right price.',
-			link: 'https://eurobridge.com.mt/',
-		},
-		{
-			image: '/work/qonetech.png',
-			title: 'Q One Tech',
-			description:
-				'Q One builds on automation and machine learning empowering research operation teams with more control over panel experience, quantitative and qualitative recruitment, surveys, and rewards.',
-			link: 'https://qone-tech.com/',
-		},
+		// {
+		// 	image: '/work/giftlips.png',
+		// 	title: 'GiftLips',
+		// 	description:
+		// 		'GiftLips takes the traditional greeting card experience to the next level.',
+		// 	link: 'https://www.giftlips.com/',
+		// },
+		// {
+		// 	image: '/work/heyweek.png',
+		// 	title: 'Heyweek',
+		// 	description:
+		// 		'Heyweek, your freelancing toolbox. Simplify your tasks, supercharge your workflow, and thrive in your independent journey',
+		// 	link: 'https://www.heyweek.com/',
+		// },
+		// {
+		// 	image: '/work/yesspark.png',
+		// 	title: 'YesSpark',
+		// 	description:
+		// 		'YesSpark inspire and encourage future entrepreneurs, founders and teams who are passionate about delivering unique solutions to their customers from Spark to Launch.',
+		// 	link: 'https://www.yesspark.io/',
+		// },
+		// {
+		// 	image: '/work/eurobridge.png',
+		// 	title: 'Eurobridge',
+		// 	description:
+		// 		'EuroBridge Shipping Services deliver the right service, at the right level of quality, and for the right price.',
+		// 	link: 'https://eurobridge.com.mt/',
+		// },
+		// {
+		// 	image: '/work/qonetech.png',
+		// 	title: 'Q One Tech',
+		// 	description:
+		// 		'Q One builds on automation and machine learning empowering research operation teams with more control over panel experience, quantitative and qualitative recruitment, surveys, and rewards.',
+		// 	link: 'https://qone-tech.com/',
+		// },
 		// {
 		//   image: '/work/apfelherz.png',
 		//   title: "Apfelherz",
