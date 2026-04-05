5904f8dac202a8f6b5da9eb875a9e84c39ceb9f9
Mitanshu <mitanshu2531@gmail.com>
2026-04-05 21:07:33 +0530

    Add Facebook domain verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="facebook-domain-verification" content="0qrvm4f9q1h61gdfq5hv4iijqdmvcx" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
